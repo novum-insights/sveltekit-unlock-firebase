1f12a79ab58f7801f4d7873168d5a7750da16e5f
Shriji <shriji@anoram.com>
2021-12-02 20:52:51 +0530

    make it heroku friendly

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,11 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-node';
-console.log(process.env.NODE_ENV)
+console.log(process.env.NODE_ENV);
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
-
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
@@ -17,22 +16,17 @@ const config = {
 		ssr: false,
 		floc: true,
 		vite: {
-
 			ssr: {
 				external: ['firebase', 'firebase-admin'],
-				noExternal: process.env.NODE_ENV === 'production' && ['ethers'],
+				noExternal: process.env.NODE_ENV === 'production' && ['ethers']
 			}
 		},
 		adapter: adapter({
 			// default options are shown
 			out: 'build',
 
-			env: {
-				host: 'HOST',
-				port: 'PORT'
-			}
+			env: { port: process.env.PORT }
 		})
-
 	}
 };
 
