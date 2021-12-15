a73250d16203b9c37fd8d085687ec876322d62a9
Shriji <shrijike@gmail.com>
2021-12-15 23:28:03 +0530

    swap ethers -> web3 only for backend

--- src/routes/_ethersAdapter.ts
+++ src/routes/_ethersAdapter.ts
@@ -1,19 +1,31 @@
-import { ethers } from 'ethers';
+import Web3 from 'web3';
+
+import type { AbiItem } from 'web3-utils';
+
 import ABI from '$lib/contract.abi.json';
-import { CONTRACT } from '$lib/web3';
+
 import { contract, rpcProvider } from '$lib/contants';
 
-const provider = ethers && new ethers.providers.JsonRpcProvider(rpcProvider);
+const provider = new Web3.providers.HttpProvider(rpcProvider);
 const CONTRACT_ADDRESS = contract;
 
 export const validKey = async (account: string) => {
-	const lock = CONTRACT(CONTRACT_ADDRESS, ABI, provider);
-	return await lock.getHasValidKey(account);
+	const web3 = new Web3(provider);
+	const lock = new web3.eth.Contract(ABI as AbiItem[], CONTRACT_ADDRESS);
+	return await lock.methods.getHasValidKey(account).call();
 };
 
-export const verifyMessage = async (message: string, signature: string) =>
-	ethers.utils.verifyMessage(message, signature);
+export const verifyMessage = async (message: string, signature: string) => {
+	const web3 = new Web3(provider);
+	return web3.eth.accounts.recover(message, signature);
+};
 
-export const transaction = async (hash: string) => await provider.getTransactionReceipt(hash);
+export const transaction = async (hash: string) => {
+	const web3 = new Web3(provider);
+	return await web3.eth.getTransactionReceipt(hash);
+};
 
-export const getBalance = async (account: string) => await provider.getBalance(account);
+export const getBalance = async (account: string) => {
+	const web3 = new Web3(provider);
+	return await web3.eth.getBalance(account);
+};
