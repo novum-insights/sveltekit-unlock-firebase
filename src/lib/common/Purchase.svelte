a60f0db64924e5e3e3e5712567ff9ece9d39e5ef
shriji <shrijike@gmail.com>
2022-02-18 12:59:23 +0530

    Purchase with ETH -> Regenerate tokens

--- src/lib/common/Purchase.svelte
+++ src/lib/common/Purchase.svelte
@@ -9,12 +9,14 @@
 	import ABI from '$lib/contract.abi.json';
 	import { base } from '$app/paths';
 	import { logOut, metamaskSignIn } from '$lib/client/firebase';
+	import { signature } from '$lib/stores';
+	import { handleSignMessage } from '$lib/web3/helpers';
 
 	const CONTRACT_ADDRESS = contract;
 
 	let lock: any, subscriptionPriceEth: any;
 	let transactionHash: string;
-
+	let debugMessage = '';
 	async function buyUnlock() {
 		lock = CONTRACT(CONTRACT_ADDRESS, ABI, provider);
 		subscriptionPriceEth = await getPrice(lock);
@@ -24,6 +26,7 @@
 		if (transactionHash) {
 			let receipt = await provider.waitForTransaction(transactionHash, 1);
 			console.log(receipt);
+			debugMessage = `The unlock requires a signature to regenrate key.`;
 			if (receipt) {
 				await setClaims(
 					{
@@ -46,7 +49,7 @@
 		);
 	}
 
-	async function setClaims(claims, reload) {
+	async function setClaims(claims: object, reload: boolean) {
 		await fetch(`${base}/api/addclaim`, {
 			method: 'POST',
 			headers: {
@@ -60,13 +63,27 @@
 			.then((e) => e.json())
 			.then((e) => {
 				console.log(e);
-				logOut({ reload }).then(() => metamaskSignIn());
+				logOut({ reload }).then(() => signWithMessage());
 			});
+	}
+	async function signWithMessage() {
+		let data = await fetch(`${base}/api/nonce`, {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify({
+				address: account
+			})
+		}).then((e) => e.json());
+		$signature = await handleSignMessage(signer, data.address, data.nonce);
+		await metamaskSignIn();
 	}
 </script>
 
 {#if subscriptionType == 'ether' && ethereum}
 	<button on:click={buyUnlock}> Purchase with ETH </button>
+	<p>{debugMessage}</p>
 {:else}
 	<button on:click={buyStripe}> Purchase with Stripe </button>
 {/if}
